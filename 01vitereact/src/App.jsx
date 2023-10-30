29e7ddc49d7f13f0710d5df8df291fccd2a1ae64
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-10-30 11:53:02 +0530

    first react project with react use state hook

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+   let [count,setCounter] = useState(0)
+
+  //  let count=0
+   let valueup =() => {
+   console.log("valueup", count)
+   if (count < 20 ){
+    count += 1
+    setCounter(count)
+   }
+  //  else{
+  //   setCounter(count)
+  //  }
+     }
+   let valuedown =() => {
+    if (count >0){
+    count -= 1
+   setCounter (count)
+    }
+   }
+  return (
+<>
+<span>Count{count}</span>
+<br />
+<button onClick={valueup}>Up {count}</button>
+    <button onClick={valuedown}>Down {count}</button>
+</>
+
+
+  )
+}
+
+export default App
